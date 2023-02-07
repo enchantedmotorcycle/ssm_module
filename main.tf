2f0fe259c4408b209c2f24e0ebad5385b8e91f5e
James Chandler <james.chandler@trace3.com>
2023-02-07 14:25:26 -0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+locals {
+  local_data = jsondecode(file("${path.module}/ssm_input.json"))
+}
+
+output "show_locals" {
+  value = local.local_data.aws_ssm_parameter
+}
+
+
+resource "aws_ssm_parameter" "secure_ssm_params" {
+  for_each = {
+    for param in local.local_data.aws_ssm_parameter : param.ssm_param_name => param if param.ssm_param_type == "SecureString"
+  }
+  name        = each.key
+  description = "The parameter description"
+  type        = "SecureString"
+  value       = data.aws_kms_secrets.secure_ssm_secrets[each.key].plaintext[each.key]
+  #value       = "temp"
+  #key_id      = "key_id"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+data "aws_kms_secrets" "secure_ssm_secrets" {
+  for_each = {
+    for param in local.local_data.aws_ssm_parameter : param.ssm_param_name => param if param.ssm_param_type == "SecureString"
+  }
+  secret {
+    name    = each.key
+    payload = each.value.ssm_param_value
+  }
+}
+
+output "show_data" {
+  value = data.aws_kms_secrets.secure_ssm_secrets
+  sensitive = true
+}
+
+resource "aws_ssm_parameter" "ssm_params" {
+  for_each = {
+    for param in local.local_data.aws_ssm_parameter : param.ssm_param_name => param if param.ssm_param_type == "String"
+  }
+  name        = each.key
+  description = "The parameter description"
+  type        = "String"
+  value       = each.value.ssm_param_value
+  #value       = "temp"
+  #key_id      = "key_id"
+
+  tags = {
+    environment = "production"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default     = "us-west-2"
+}
+
+variable "data_type" {
+  default     = "text"
+}
+
+variable "key_id" {
+}
+
+variable "ssm_parameter" {
+  default = "blank"
+}
+
+variable "input_file" {
+  default     = "ssm_input.json"
+  description = "Input file with AWS SSM Parameters."
+  #type        = list(string)
+  validation {
+    condition     = length(var.input_file) > 1
+    error_message = "Input file path cannot be blank"
+  }
+  validation {
+    condition     = length(var.ssm_parameter) < 1
+    error_message = "Cannot specify both an input file and a parameter"
+  }
+}
