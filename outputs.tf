7e102f55af9bd5ceb8406412f0245c6b79ce0d31
James Chandler <james.chandler@trace3.com>
2023-02-09 12:34:14 -0800

    module outputs 1

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "show_data" {
 output "show_locals" {
   value = local.local_data.aws_ssm_parameter
 }
+
+output "parameters" {
+  value = aws_ssm_parameter.ssm_params
+}
+
+output "secure_parameters" {
+  value = aws_ssm_parameter.secure_ssm_params
+}
