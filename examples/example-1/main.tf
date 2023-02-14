b3aafbabbe840114ebea9bf92b75d87b4596535c
James Chandler <james.chandler@trace3.com>
2023-02-13 17:38:34 -0800

    minor update docs syntax

--- examples/example-1/main.tf
+++ examples/example-1/main.tf
@@ -1,7 +1,7 @@
 # Create SSM parameters using a JSON input file
 module my_parameters_from_file {
     source = "git::https://github.com/path_to_module/ssm_module.git"
-    input_file = "ssm_input_json"
+    input_file = "ssm_input.json"
     key_id = "arn:aws:kms:us-west-2:2309209292:key/21ba1676-db71-4c0e-b8be-xxxxxxxx"
     tags = {env = "non-prod", paramset = "my tag value"}
 }
